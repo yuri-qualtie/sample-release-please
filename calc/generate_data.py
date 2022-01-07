c4cefda00219a76826d4901e540f7749fa8ef0df
Yuri Goncharov <yurig@qualtie.com>
2022-01-07 15:03:58 +0300

    initial revision

---
+++ calc/generate_data.py
@@ -0,0 +1,18 @@
+
+import os
+import datetime
+
+txt_dir = os.getcwd() + '/txt'
+
+def generate_and_save(filename):
+    with open(filename, 'w') as file:
+        now = datetime.datetime.now()
+        file.write(now.strftime("%d-%m-%Y %H:%M:%S"))
+
+data_count = 100
+
+for i in range(data_count):
+    filename = txt_dir + '/test' + str(i) + '.txt'
+    generate_and_save(filename)
+
+print('Generated ' + str(data_count) + ' files')
